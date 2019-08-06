8bef8a1ce9458f714e47ea168b915173d9ea6333
Jonatan Tisnado <jonatan.tisnado@gmail.com>
2019-08-06 15:30:40 -0500

    Created new file add-new-properties-to-a-javascript-object.js
    ladrido

---
+++ js/add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,24 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "woof";
+
+var ladrido = myDog.bark;
+var mostrar = document.getElementById("js_mostrado");
+mostrar.innerText = ladrido;
