15dae81e756520109d019d3cdb914e75a536e81e
Jonatan Tisnado <jonatan.tisnado@gmail.com>
2019-08-06 15:27:54 -0500

    updated updating-object-properties.js
    Added perro to Happy Coder so it reads PerroHappy Coder.

--- js/updating-object-properties.js
+++ js/updating-object-properties.js
@@ -18,7 +18,7 @@ var myDog = {
 
 // Only change code below this line.
 
-myDog.name = "Happy Coder";
+myDog.name = "Perro Happy Coder";
 // Display
 var miPerro = myDog.name;
 var mostraralperro = document.getElementById("js_mostrado");
